31538aba692a449d79eb6428d1466316697ef177
micouy <szpontaniczny@gmail.com>
2021-09-13 20:14:51 +0200

    Remove all code

--- src/init/mod.rs
+++
@@ -1,9 +0,0 @@
-use crate::args::Shell;
-
-pub fn init(shell: Shell) -> &'static str {
-    match shell {
-        Shell::Fish => include_str!("../../init/kn.fish.template"),
-        Shell::Zsh => include_str!("../../init/kn.zsh.template"),
-        Shell::Bash => include_str!("../../init/kn.bash.template"),
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,6 @@
-#![allow(unused_parens)]
-#![allow(clippy::let_and_return)]
+#![warn(missing_docs)]
 
-use std::{fs, process::exit};
-
-#[macro_use]
-mod utils;
-// mod app;
-mod args;
-mod error;
-mod search;
-
-mod init;
-mod interactive;
-mod query;
-
-pub use error::{Error, Result};
-
-use args::{parse_args, Subcommand};
+mod abbr;
 
 fn main() {
-    match _main() {
-        Err(error) => {
-            eprintln!("{}", error);
-            exit(1);
-        }
-        Ok(()) => exit(0),
-    }
-}
-
-fn _main() -> Result<()> {
-    let subcommand = parse_args()?;
-
-    match subcommand {
-        Subcommand::Init { shell } => {
-            let script = init::init(shell);
-            print!("{}", script);
-
-            Ok(())
-        }
-        Subcommand::Query { abbr } => match query::query(abbr) {
-            Err(error) => Err(error),
-            Ok(path) => {
-                println!("{}", path.display());
-
-                Ok(())
-            }
-        },
-        Subcommand::Interactive { tmpfile } => {
-            let found_path = interactive::interactive()?;
-            let found_path =
-                found_path.to_str().ok_or(dev_err!("invalid Unicode"))?;
-            fs::write(tmpfile, found_path)?;
-
-            Ok(())
-        }
-    }
 }
