99af2c997014b8def2c2d736c914b73b1823e238
micouy <szpontaniczny@gmail.com>
2022-07-17 19:46:01 +0200

    Fix `--exclude-old-pwd` in fish

--- init/kn.fish
+++ init/kn.fish
@@ -9,9 +9,11 @@ function kn
 
         cd
     else if begin; test $argc -eq 1; and test "$argv[1]" = '-'; end
-        # only dash provided, go to previous location
+        # only dash provided, go to previous location if it exists
 
-        cd -
+        #if test (count $dirprev) -ne 0
+        cd $dirprev[-1]
+        # end
     else
         # otherwise, query _kn
 
